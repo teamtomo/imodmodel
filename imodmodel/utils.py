39a53ff617d41605af1bd4e8302ed42c1551a293
Alister Burt <alisterburt@gmail.com>
2022-07-19 20:49:55 +0100

    add utils for clean read function

---
+++ imodmodel/utils.py
@@ -0,0 +1,29 @@
+from collections import deque
+import pandas as pd
+
+from .data_structures import Model, Contour
+
+
+def model_to_dataframe(model: Model) -> pd.DataFrame:
+    """Convert Model model into a pandas DataFrame."""
+    contour_dfs = deque()
+    for object_idx, object in enumerate(model.objects):
+        for contour_idx, contour in enumerate(object.contours):
+            contour_df = contour_to_dataframe(contour, object_idx, contour_idx)
+            contour_dfs.append(contour_df)
+    return pd.concat(contour_dfs)
+
+
+def contour_to_dataframe(
+        contour: Contour, object_id: int, contour_id: int
+) -> pd.DataFrame:
+    """Convert contour model into a pandas DataFrame."""
+    n_points = len(contour.points)
+    contour_data = {
+        'object_id': [object_id for _ in range(n_points)],
+        'contour_id': [contour_id for _ in range(n_points)],
+        'x': contour.points[:, 0],
+        'y': contour.points[:, 1],
+        'z': contour.points[:, 2],
+    }
+    return pd.DataFrame(contour_data)
