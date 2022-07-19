f03919a3d75ec10c463642ceb45fe7d829a2ce58
Alister Burt <alisterburt@gmail.com>
2022-07-19 20:51:24 +0100

    add test for read function

---
+++ tests/test_functions.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+from imodmodel.functions import read
+
+
+def test_read(two_contour_model_file):
+    """Check that model files can be read."""
+    df = read(two_contour_model_file)
+    assert isinstance(df, pd.DataFrame)
+    expected_columns = ['object_id', 'contour_id', 'x', 'y', 'z']
+    assert all(col in expected_columns for col in df.columns)
+    assert df.shape == (25, 5)
+    assert df['object_id'].nunique() == 1
+    assert df['contour_id'].nunique() == 2
