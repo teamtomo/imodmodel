9693cc53899443255b464bda7f09c40070645131
alisterburt <alisterburt@gmail.com>
2023-01-04 12:27:30 +0000

    lint

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -3,9 +3,16 @@ from typing import Dict
 import numpy as np
 import pytest
 
-from imodmodel.parsers import _parse_id, _parse_model_header, \
-    _parse_control_sequence, \
-    _parse_object_header, _parse_contour, _parse_imat, _parse_chunk_size, parse_model
+from imodmodel.parsers import (
+    _parse_id,
+    _parse_model_header,
+    _parse_control_sequence,
+    _parse_object_header,
+    _parse_contour,
+    _parse_imat,
+    _parse_chunk_size,
+    parse_model,
+)
 
 
 def test_parse_id(two_contour_model_file_handle):
@@ -64,7 +71,7 @@ def test_parse_model_header(two_contour_model_file_handle):
     ]
 )
 def test_parse_control_sequence(
-        two_contour_model_file_handle, position: int, expected: str
+    two_contour_model_file_handle, position: int, expected: str
 ):
     """Check that control sequences are correctly parsed."""
     two_contour_model_file_handle.seek(position)
@@ -106,45 +113,45 @@ def test_parse_object_header(two_contour_model_file_handle):
     'position, expected_header, expected_points',
     [
         (
-                424,
-                {'flags': 0, 'psize': 17, 'surf': 0, 'time': 0},
-                np.array([[64.33333588, 64.66666412, 80.],
-                          [47., 77.33333588, 80.],
-                          [51.33333206, 45.66666794, 80.],
-                          [87.33333588, 49.66666794, 80.],
-                          [76., 82., 80.],
-                          [34.33333206, 84.33333588, 80.],
-                          [40.66666794, 31., 80.],
-                          [97.33333588, 45.66666794, 80.],
-                          [81., 93.33333588, 80.],
-                          [20., 93.66666412, 80.],
-                          [32.33333206, 16.33333397, 80.],
-                          [110., 41.33333206, 80.],
-                          [86., 102., 80.],
-                          [10.66666698, 107.33333588, 80.],
-                          [23.66666603, 4.33333349, 80.],
-                          [121.66666412, 37., 80.],
-                          [96.33333588, 110.33333588, 80.]])
+            424,
+            {'flags': 0, 'psize': 17, 'surf': 0, 'time': 0},
+            np.array([[64.33333588, 64.66666412, 80.],
+                      [47., 77.33333588, 80.],
+                      [51.33333206, 45.66666794, 80.],
+                      [87.33333588, 49.66666794, 80.],
+                      [76., 82., 80.],
+                      [34.33333206, 84.33333588, 80.],
+                      [40.66666794, 31., 80.],
+                      [97.33333588, 45.66666794, 80.],
+                      [81., 93.33333588, 80.],
+                      [20., 93.66666412, 80.],
+                      [32.33333206, 16.33333397, 80.],
+                      [110., 41.33333206, 80.],
+                      [86., 102., 80.],
+                      [10.66666698, 107.33333588, 80.],
+                      [23.66666603, 4.33333349, 80.],
+                      [121.66666412, 37., 80.],
+                      [96.33333588, 110.33333588, 80.]])
         ),
         (
-                648,
-                {'flags': 0, 'psize': 8, 'surf': 0, 'time': 0},
-                np.array([[64.33333588, 64., 59.],
-                          [56., 52.66666794, 59.],
-                          [85.33333588, 47.66666794, 59.],
-                          [77., 77., 59.],
-                          [45., 80., 59.],
-                          [48.33333206, 48.66666794, 59.],
-                          [96.33333588, 43., 59.],
-                          [83., 82., 59.]])
+            648,
+            {'flags': 0, 'psize': 8, 'surf': 0, 'time': 0},
+            np.array([[64.33333588, 64., 59.],
+                      [56., 52.66666794, 59.],
+                      [85.33333588, 47.66666794, 59.],
+                      [77., 77., 59.],
+                      [45., 80., 59.],
+                      [48.33333206, 48.66666794, 59.],
+                      [96.33333588, 43., 59.],
+                      [83., 82., 59.]])
         )
     ]
 )
 def test_parse_contour(
-        two_contour_model_file_handle,
-        position: int,
-        expected_header: Dict[str, int],
-        expected_points: np.ndarray,
+    two_contour_model_file_handle,
+    position: int,
+    expected_header: Dict[str, int],
+    expected_points: np.ndarray,
 ):
     """Check that contours are correctly parsed."""
     two_contour_model_file_handle.seek(position)
