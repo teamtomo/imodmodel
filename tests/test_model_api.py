7df31dec6b80d504937ba26a8408a6dea928ab34
alisterburt <alisterburt@gmail.com>
2024-12-11 15:28:51 -0800

    Update tests/test_model_api.py

--- tests/test_model_api.py
+++ tests/test_model_api.py
@@ -90,4 +90,3 @@ def test_read_write_read_roundtrip(two_contour_model_file, tmp_path):
     assert np.allclose(model.objects[0].contours[0].points, model2.objects[0].contours[0].points)
     assert model.objects[0].contours[1].header == model2.objects[0].contours[1].header
     assert np.allclose(model.objects[0].contours[1].points, model2.objects[0].contours[1].points)
-    
