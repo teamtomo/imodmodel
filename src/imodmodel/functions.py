4e500f8c3da3d40fbdb7ed28e6ec4a20d8b01acd
Johannes Elferich <jojotux123@hotmail.com>
2025-04-04 16:10:10 -0400

    Pass type option to functional API

--- src/imodmodel/functions.py
+++ src/imodmodel/functions.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from .models import ImodModel
+from .models import ImodModel, ContourType
 from .dataframe import model_to_dataframe
 
 
@@ -15,7 +15,7 @@ def read(filename: os.PathLike, annotation: str = 'contour') -> pd.DataFrame:
     model = ImodModel.from_file(filename)
     return model_to_dataframe(model,annotation)
 
-def write(dataframe: pd.DataFrame, filename: os.PathLike) -> None:
+def write(dataframe: pd.DataFrame, filename: os.PathLike, type: ContourType=ContourType.SCATTERED) -> None:
     """Write a pandas DataFrame to an IMOD model file.
 
     Parameters
@@ -23,5 +23,5 @@ def write(dataframe: pd.DataFrame, filename: os.PathLike) -> None:
     dataframe : pandas DataFrame to write
     filename : filename to write
     """
-    model = ImodModel.from_dataframe(dataframe)
+    model = ImodModel.from_dataframe(dataframe, type=type)
     model.to_file(filename)
